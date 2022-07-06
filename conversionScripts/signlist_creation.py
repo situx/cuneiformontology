31e97b1706152a795830ae524944af8e01af2b86
Timo <situx@users.noreply.github.com>
2022-07-06 19:39:50 +0200

    Initial signlist

--- conversionScripts/signlist_creation.py
+++ conversionScripts/signlist_creation.py
@@ -14,20 +14,16 @@ def convertToRDF(cuneiformsigndict,nuolenna,rdfset):
         signuri=cleanString(entry["signname"])
         rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> rdf:type graphemon:Grapheme .\n")
         rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> rdfs:label \"Character: "+str(entry["signname"]).replace("\"","")+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:meszl \""+str(entry["meszl"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:slha \""+str(entry["slha"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:hethzl \""+str(entry["hethzl"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:abzl \""+str(entry["abzl"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:unicodeCodepoint \""+str(entry["unicode"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:unicodeRepresentation \""+str(entry["unicodename"])+"\" . \n")
-        unicodeToURI[entry["unicode"]]="http://purl.org/cuneiform/signlist/character_"+str(signuri)
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:meszl \""+str(entry["meszl"]).replace("\"","")+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:slha \""+str(entry["slha"]).replace("\"","")+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:hethzl \""+str(entry["hethzl"]).replace("\"","")+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:abzl \""+str(entry["abzl"]).replace("\"","")+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:unicodeCodepoint \""+str(entry["unicode"]).replace("\"","")+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:unicodeRepresentation \""+str(entry["unicodename"]).replace("\"","")+"\" . \n")
+        unicodeToURI[entry["unicodename"]]="http://purl.org/cuneiform/signlist/character_"+str(signuri)
     print(unicodeToURI)
     for item in nuolenna:
-        print(item)
-        print(nuolenna[item])
-        if len(nuolenna[item])>1:
-            print("todo")
-        elif len(nuolenna[item])==1 and nuolenna[item] in unicodeToURI:
+        if len(nuolenna[item])==1 and nuolenna[item] in unicodeToURI:
             cururi=unicodeToURI[nuolenna[item]]
             rdfset.add("<"+str(cururi)+"> graphemon:hasGraphemeReading <"+str(cururi)+"_reading_"+cleanString(str(item))+"> .\n")
             rdfset.add("<"+str(cururi)+"_reading_"+cleanString(str(item))+"> rdf:type graphemon:GraphemeReading .\n")
@@ -54,10 +50,20 @@ print(cuneiformsigndict)
 
 convertToRDF(cuneiformsigndict,nuolenna,rdfset)
 
-with open('../signlist.ttl', mode='w', encoding="utf-8") as f:
-    f.write("@prefix graphemon:<http://purl.org/graphemon> .\n")
-    f.write("@prefix rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n")
-    f.write("@prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#> .\n")
+with open('../signlist/signlist.ttl', mode='w', encoding="utf-8") as f:
+    f.write("@prefix graphemon: <http://purl.org/graphemon/> .\n")
+    f.write("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n")
+    f.write("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n")
+    f.write("@prefix owl: <http://www.w3.org/2002/07/owl#> .\n")
+    f.write("graphemon:GraphemeReading rdf:type owl:Class .\n")
+    f.write("graphemon:Grapheme rdf:type owl:Class .\n")
+    f.write("graphemon:hasGraphemeReading rdf:type owl:ObjectProperty .\n")
+    f.write("graphemon:hethzl rdf:type owl:DatatypeProperty .\n")
+    f.write("graphemon:abzl rdf:type owl:DatatypeProperty .\n")
+    f.write("graphemon:meszl rdf:type owl:DatatypeProperty .\n")
+    f.write("graphemon:slha rdf:type owl:DatatypeProperty .\n")
+    f.write("graphemon:unicodeCodepoint rdf:type owl:DatatypeProperty .\n")
+    f.write("graphemon:unicodeRepresentation rdf:type owl:DatatypeProperty .\n")
     f.write("".join(rdfset))
 
 
