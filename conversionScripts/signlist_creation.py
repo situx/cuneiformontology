e1bb1c22f9653b89dc83db91f51d3f01cf9335f1
Timo <situx@users.noreply.github.com>
2022-07-06 19:26:35 +0200

    update

--- conversionScripts/signlist_creation.py
+++ conversionScripts/signlist_creation.py
@@ -4,31 +4,44 @@ import json
 
 rdfset=set()
 
+def cleanString(strr):
+    return strr.lower().replace("š","sz").replace(" ","_").replace("\"","").replace("|","_").replace("/","_").replace("+","_").replace("(","_").replace(")","_").replace(".","_").replace(":","_")
+
 def convertToRDF(cuneiformsigndict,nuolenna,rdfset):
     unicodeToURI={}
     for entry in cuneiformsigndict:
         print(entry)
-        signuri=entry["signname"].lower().replace("š","sz")
-        rdfset.add("<http://purl.org/cuneiform/signlist/"+str(signuri)+"> rdf:type cunei:Grapheme .\n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/"+str(signuri)+"> rdfs:label \"Character: "+str(entry["signname"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/"+str(signuri)+"> cunei:meszl \""+str(entry["meszl"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/"+str(signuri)+"> cunei:slha \""+str(entry["slha"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/"+str(signuri)+"> cunei:hethzl \""+str(entry["hethzl"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/"+str(signuri)+"> cunei:abzl \""+str(entry["abzl"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/"+str(signuri)+"> cunei:unicodeCodepoint \""+str(entry["unicode"])+"\" . \n")
-        rdfset.add("<http://purl.org/cuneiform/signlist/"+str(signuri)+"> cunei:unicodeRepresentation \""+str(entry["unicodename"])+"\" . \n")
-        unicodeToURI[entry["unicode"]]="http://purl.org/cuneiform/signlist/"+str(signuri)
+        signuri=cleanString(entry["signname"])
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> rdf:type graphemon:Grapheme .\n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> rdfs:label \"Character: "+str(entry["signname"]).replace("\"","")+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:meszl \""+str(entry["meszl"])+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:slha \""+str(entry["slha"])+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:hethzl \""+str(entry["hethzl"])+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:abzl \""+str(entry["abzl"])+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:unicodeCodepoint \""+str(entry["unicode"])+"\" . \n")
+        rdfset.add("<http://purl.org/cuneiform/signlist/character_"+str(signuri)+"> graphemon:unicodeRepresentation \""+str(entry["unicodename"])+"\" . \n")
+        unicodeToURI[entry["unicode"]]="http://purl.org/cuneiform/signlist/character_"+str(signuri)
     print(unicodeToURI)
-    #print(rdfset)
+    for item in nuolenna:
+        print(item)
+        print(nuolenna[item])
+        if len(nuolenna[item])>1:
+            print("todo")
+        elif len(nuolenna[item])==1 and nuolenna[item] in unicodeToURI:
+            cururi=unicodeToURI[nuolenna[item]]
+            rdfset.add("<"+str(cururi)+"> graphemon:hasGraphemeReading <"+str(cururi)+"_reading_"+cleanString(str(item))+"> .\n")
+            rdfset.add("<"+str(cururi)+"_reading_"+cleanString(str(item))+"> rdf:type graphemon:GraphemeReading .\n")
+            rdfset.add("<"+str(cururi)+"_reading_"+cleanString(str(item))+"> rdfs:label \"Grapheme Reading: "+str(item).replace("\"","")+"\" .\n")
+            rdfset.add("<"+str(cururi)+"_reading_"+cleanString(str(item))+"> graphemon:readingValue \""+str(item).replace("\"","")+"\" .\n")
 
-with open('../signlist/nuolenna.json') as f:
+with open('../signlist/nuolenna.json', encoding="utf-8") as f:
    nuolenna = json.load(f)
 
 print(nuolenna)
 
 cuneiformsigndict = []
 
-with open('../signlist/listofcuneiformsigns_wikipedia.csv', mode='r') as inp:
+with open('../signlist/listofcuneiformsigns_wikipedia.csv', mode='r', encoding="utf-8") as inp:
     reader = csv.reader(inp)
     counter=0
     for rows in reader:
@@ -41,7 +54,10 @@ print(cuneiformsigndict)
 
 convertToRDF(cuneiformsigndict,nuolenna,rdfset)
 
-with open('../signlist.ttl', 'w') as f:
+with open('../signlist.ttl', mode='w', encoding="utf-8") as f:
+    f.write("@prefix graphemon:<http://purl.org/graphemon> .\n")
+    f.write("@prefix rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n")
+    f.write("@prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#> .\n")
     f.write("".join(rdfset))
 
 
