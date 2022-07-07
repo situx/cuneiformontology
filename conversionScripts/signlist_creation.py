5ff02bc082595272a82cab2bff9bcf6fd7e8a0ef
Timo <situx@users.noreply.github.com>
2022-07-07 15:06:23 +0200

    update

--- conversionScripts/signlist_creation.py
+++ conversionScripts/signlist_creation.py
@@ -5,8 +5,57 @@ from rdflib import Graph
 
 rdfset=set()
 
+sensesmap={
+    "life":"http://www.wikidata.org/entity/Q3",
+    "district":"http://www.wikidata.org/entity/Q149621",
+    "flesh":"http://www.wikidata.org/entity/Q13119823",
+    "throat":"http://www.wikidata.org/entity/Q16364",
+    "country":"http://www.wikidata.org/entity/Q6256",
+    "barley":"http://www.wikidata.org/entity/Q61665121",
+    "raisin":"http://www.wikidata.org/entity/Q13186",
+    "wing:":"http://www.wikidata.org/entity/Q161358",
+    "branch":"http://www.wikidata.org/entity/Q2923673",
+    "water":"http://www.wikidata.org/entity/Q283",
+    "fire":"http://www.wikidata.org/entity/Q3196",
+    "prince":"http://www.wikidata.org/entity/Q2747456",
+    "morning":"http://www.wikidata.org/entity/Q7722",
+    "breath":"http://www.wikidata.org/entity/Q13428325",
+    "hand":"http://www.wikidata.org/entity/Q33767",
+    "tendon":"http://www.wikidata.org/entity/Q232358",
+    "horn":"http://www.wikidata.org/entity/Q82025",
+    "egg":"http://www.wikidata.org/entity/Q93189",
+    "tail":"http://www.wikidata.org/entity/Q60960",
+    "road":"http://www.wikidata.org/entity/Q34442",
+    "caravan":"http://www.wikidata.org/entity/Q203494",
+    "clay":"http://www.wikidata.org/entity/Q42302",
+    "night":"http://www.wikidata.org/entity/Q575",
+    "year":"http://www.wikidata.org/entity/Q577",
+    "snake":"http://www.wikidata.org/entity/Q2102",
+    "king":"http://www.wikidata.org/entity/Q116",
+    "sand":"http://www.wikidata.org/entity/Q34679",
+    "dirt":"http://www.wikidata.org/entity/Q21152267",
+    "garden":"http://www.wikidata.org/entity/Q1107656",
+    "scepter":"http://www.wikidata.org/entity/Q152065",
+    "dust":"http://www.wikidata.org/entity/Q165632",
+    "ox":"http://www.wikidata.org/entity/Q473194",
+    "steppe":"http://www.wikidata.org/entity/Q123991",
+    "net":"http://www.wikidata.org/entity/Q986291",
+    "shrine":"http://www.wikidata.org/entity/Q697295",
+    "sky":"http://www.wikidata.org/entity/Q527",
+    "heaven":"http://www.wikidata.org/entity/Q4489450",
+    "slave":"http://www.wikidata.org/entity/Q12773225",
+    "servant":"http://www.wikidata.org/entity/Q54128",
+    "lake":"http://www.wikidata.org/entity/Q23397",
+    "leatherworker":"http://www.wikidata.org/entity/Q16402902",
+    "tongue":"http://www.wikidata.org/entity/Q9614",
+    "linen":"http://www.wikidata.org/entity/Q1426327",
+    "milk":"http://www.wikidata.org/entity/Q8495"
+    
+    
+}
+
 def cleanString(strr):
-    return strr.lower().replace("š","sz").replace("Š","SZ").replace("%","_").replace(" ","_").replace("'","_").replace("\"","").replace("|","_").replace("/","_").replace("-","_").replace("+","_").replace("%","_").replace("(","_").replace(")","_").replace(".","_").replace(":","_").replace("₄","4").replace("₂","2").replace("₃","3").replace("₅","5").replace("₆","6").replace("₈","8").replace("₉","9").replace("₁","1").replace("₀","0")
+    return strr.lower().replace("š","sz").replace("Š","SZ").replace("%","_").replace(" ","_").replace("'","_").replace("\"","").replace(",","_").replace("|","_").replace("/","_").replace("-","_").replace("+","_").replace("%","_").replace("(","_").replace(")","_").replace(".","_").replace(":","_").replace("₄","4").replace("₂","2").replace("₃","3").replace("₅","5").replace("₆","6").replace("₈","8").replace("₉","9").replace("₁","1").replace("₀","0")
 
 def toASCII(strr):
     return strr.replace("₄","4").replace("₂","2").replace("₃","3").replace("₅","5").replace("₇","7").replace("₆","6").replace("₈","8").replace("₉","9").replace("₁","1").replace("₀","0").replace("%","_").replace("š","sz").replace("Š","SZ")
@@ -36,10 +85,13 @@ def convertToRDF(cuneiformsigndict,nuolenna,aasigndict,rdfset):
         if nuolenna[item] in unicodeToURI:
             cururi=unicodeToURI[nuolenna[item]]["uri"]
             cursignname=unicodeToURI[nuolenna[item]]["signname"]
-            rdfset.add("<"+str(cururi)+"> graphemon:hasGraphemeReading <"+str(cururi)+"_reading_"+cleanString(str(item))+"> .\n ")
-            rdfset.add("<"+str(cururi)+"_reading_"+cleanString(str(item))+"> rdf:type graphemon:GraphemeReading .\n ")
-            rdfset.add("<"+str(cururi)+"_reading_"+cleanString(str(item))+"> rdfs:label \"Grapheme Reading "+str(cursignname)+": "+toASCII(str(item)).replace("\"","")+"\" .\n ")
-            rdfset.add("<"+str(cururi)+"_reading_"+cleanString(str(item))+"> graphemon:readingValue \""+toASCII(str(item)).replace("\"","")+"\" .\n ")
+            readinguri=(str(cururi)+"_reading_"+cleanString(str(item))).replace("__","_").replace("__","_")
+            if readinguri[-1]=="_":
+                readinguri=readinguri[0:-1]
+            rdfset.add("<"+str(cururi)+"> graphemon:hasGraphemeReading <"+readinguri+"> .\n ")
+            rdfset.add("<"+readinguri+"> rdf:type graphemon:GraphemeReading .\n ")
+            rdfset.add("<"+readinguri+"> rdfs:label \"Grapheme Reading "+str(cursignname)+": "+toASCII(str(item)).replace("\"","")+"\" .\n ")
+            rdfset.add("<"+readinguri+"> graphemon:readingValue \""+toASCII(str(item)).replace("\"","")+"\" .\n ")
             nuolennamatchcounter+=1
     print("Matched "+str(nuolennamatchcounter)+" items in nuolenna!")
     aalistmatchcounter=0
@@ -51,38 +103,45 @@ def convertToRDF(cuneiformsigndict,nuolenna,aasigndict,rdfset):
             rdfset.add("<"+str(cururi)+"> graphemon:obo \""+str(entry["obo"])+"\" .\n ")
             rdfset.add("<"+str(cururi)+"> graphemon:lak \""+str(entry["lak"])+"\" .\n ")
             if "meaning" in entry and entry["meaning"]!="":
-                meanings=entry["meaning"]
+                meanings=entry["meaning"].replace("v.","").replace("n.","")
                 if ";" in entry["meaning"]:
                     meanings=entry["meaning"].split(";")
                 else:
                     meanings=[entry["meaning"]]
                 for mean in meanings:
                     rdfset.add("<"+str(cururi)+"> lemon:sense <"+str(cururi)+"_sense_"+cleanString(str(mean))+"> .\n ")
-                    rdfset.add("<"+str(cururi)+"_sense_"+cleanString(str(mean))+"> lemon:writtenRep \""+str(mean).replace("\"","")+"\" .\n ")
-                    rdfset.add("<"+str(cururi)+"_sense_"+cleanString(str(mean))+"> rdfs:label \"Grapheme Sense "+str(sname)+": "+str(mean).replace("\"","")+"\" .\n ")
+                    rdfset.add("<"+str(cururi)+"_sense_"+cleanString(str(mean))+"> rdfs:label \"Grapheme Sense "+str(sname)+": "+str(mean).replace("\"","").strip()+"\" .\n ")
                     rdfset.add("<"+str(cururi)+"_sense_"+cleanString(str(mean))+"> rdf:type graphemon:GraphemeSense .\n ")
+                    print(str(mean).replace("\"","").strip())
+                    if str(mean).replace("\"","").strip() in sensesmap:
+                        rdfset.add("<"+str(cururi)+"_sense_"+cleanString(str(mean))+"> lemon:reference <"+str(sensesmap[str(mean).replace("\"","").strip()])+"> .\n ")
+                        rdfset.add("<"+str(sensesmap[str(mean).replace("\"","").strip()])+"> rdfs:label \"Wikidata: "+str(mean).replace("\"","").strip()+"\" .\n ")
             terms=[{"term":"oldbab","label":"Old Babylonian","uri":"http://www.wikidata.org/entity/Q733897"},{"term":"lagasz2","label":"Lagasz2","uri":""},{"term":"edIII","label":"EdIII","uri":"http://www.wikidata.org/entity/Q110209565"},{"term":"oldass","label":"Old Assyrian","uri":"http://www.wikidata.org/entity/Q22948607"},{"term":"middleass","label":"Middle Assyrian","uri":""},{"term":"middlebab","label":"Middle Babylonian","uri":"http://www.wikidata.org/entity/Q16530848"}]
             for term in terms:
-                print(term["term"])
+                #print(term["term"])
                 if term["term"] in entry and entry[term["term"]]!="":
                     readings=entry[term["term"]]
                     if ";" in entry[term["term"]]:
                         readings=entry[term["term"]].split(";")
                     else:
                         readings=[entry[term["term"]]]
-                    print(readings)
+                    #print(readings)
                     for reading in readings:
-                        print(str(cururi)+"_sense_"+cleanString(str(mean)))
+                        if reading.strip()=="":
+                            continue
+                        #print(str(cururi)+"_sense_"+cleanString(str(mean)))
+                        readinguri=(str(cururi)+"_reading_"+cleanString(str(item))).replace("__","_").replace("__","_")
+                        if readinguri[-1]=="_":
+                            readinguri=readinguri[0:-1]
                         if "uri" in term:
-                            rdfset.add("<"+str(cururi)+"_reading_"+cleanString(str(reading))+"> graphemon:epoch <"+str(term["uri"])+"> .\n ")
+                            rdfset.add("<"+readinguri+"> graphemon:epoch <"+str(term["uri"])+"> .\n ")
                             rdfset.add("<"+str(term["uri"])+"> rdf:type cunei:Epoch .\n ")
                             if "label" in term and term["label"]!="":
                                 rdfset.add("<"+str(term["uri"])+"> rdfs:label \"Wikidata: "+str(term["label"])+"\" .\n ")
                             else:
                                 rdfset.add("<"+str(term["uri"])+"> rdfs:label \"Wikidata: "+str(term["term"])+"\" .\n ")
-                            rdfset.add("<"+str(cururi)+"_reading_"+cleanString(str(reading))+"> graphemon:epochId \""+str(term["term"])+"\" .\n ")
                         else:
-                            rdfset.add("<"+str(cururi)+"_reading_"+cleanString(str(reading))+"> graphemon:epochId \""+str(term["term"])+"\" .\n ")
+                            rdfset.add("<"+readinguri+"> graphemon:epochId \""+str(term["term"])+"\" .\n ")
             aalistmatchcounter+=1
     print("Matched "+str(aalistmatchcounter)+" items in aalist")
 
@@ -133,7 +192,8 @@ with open('../signlist/signlist.ttl', mode='w', encoding="utf-8") as f:
     f.write("graphemon:hasGraphemeReading rdf:type owl:ObjectProperty .\n ")
     f.write("graphemon:hethzl rdf:type owl:DatatypeProperty .\n ")
     f.write("lemon:writtenRep rdf:type owl:DatatypeProperty .\n ")
-    f.write("lemon:sense rdf:type owl:ObjectProperty .\n ")
+    f.write("lemon:sense rdf:type owl:ObjectProperty .\n ")    
+    f.write("lemon:reference rdf:type owl:ObjectProperty .\n ")
     f.write("graphemon:abzl rdf:type owl:DatatypeProperty .\n ")
     f.write("graphemon:epoch rdf:type owl:ObjectProperty .\n ")
     f.write("graphemon:lak rdf:type owl:DatatypeProperty .\n ")
