9a35a7739e048193bee690108d82c2f1cbec475d
situx <situx@users.noreply.github.com>
2022-07-14 00:28:55 +0000

    Deploying to gh-pages from @ situx/cuneiformontology@7e7cc4be883b10d5a2a58e22a889be462a305902 🚀

---
+++ imageanno/js/newurls.js
@@ -0,0 +1,339 @@
+var urls={
+  "HS_1174": {
+    "label": "HS_1174",
+    "variants": [
+      {
+        "label": "HS_1174_01_top.png",
+        "url": "https://heidicon.ub.uni-heidelberg.de/iiif/2/1113342:577527/full/full/0/default.jpg"
+      },
+      {
+        "label": "HS_1174_02_left.png",
+        "url": "https://heidicon.ub.uni-heidelberg.de/iiif/2/1113343:577528/full/full/0/default.jpg"
+      },
+      {
+        "label": "HS_1174_03_front.png",
+        "url": "https://heidicon.ub.uni-heidelberg.de/iiif/2/1113344:577529/full/full/0/default.jpg"
+      },
+      {
+        "label": "HS_1174_04_right.png",
+        "url": "https://heidicon.ub.uni-heidelberg.de/iiif/2/1113345:577530/full/full/0/default.jpg"
+      },
+      {
+        "label": "HS_1174_05_bottom.png",
+        "url": "https://heidicon.ub.uni-heidelberg.de/iiif/2/1113346:577531/full/full/0/default.jpg"
+      },
+      {
+        "label": "HS_1174_06_back.png",
+        "url": "https://heidicon.ub.uni-heidelberg.de/iiif/2/1113347:577532/full/full/0/default.jpg"
+      }
+    ]
+  },
+    "O 0147 full":{
+		"label":"O 0147 full",
+		"variants":[
+		   {
+			"label": "O_0147_ColorA_full.png",
+			"url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FFatCross%2F0.0147_ColorA.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+			},
+		   {
+			"label": "O_0147_ColorB_full.png",
+			"url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FFatCross%2F0.0147_ColorB.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+			},
+		   {
+			"label": "O_0147_ColorC_full.png",
+			"url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FFatCross%2F0.0147_ColorC.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+			},
+		   {
+			"label": "O_0147_ColorD_full.png",
+			"url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FFatCross%2F0.0147_ColorD.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+			},
+		   {
+			"label": "O_0147_ShadedA_full.png",
+			"url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FFatCross%2F0.0147_ShadedA.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+			},
+		   {
+			"label": "O_0147_ShadedB_full.png",
+			"url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FFatCross%2F0.0147_ShadedB.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+			},
+		   {
+			"label": "O_0147_ShadedC_full.png",
+			"url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FFatCross%2F0.0147_ShadedC.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+			},
+		   {
+			"label": "O_0147_ShadedD_full.png",
+			"url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FFatCross%2F0.0147_ShadedD.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+			},
+		   {
+			"label": "O_0147_Sketch01Hard_full.png",
+			"url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FFatCross%2F0.0147_Sketch01Hard.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+			},
+		   {
+			"label": "O_0147_Sketch01Soft_full.png",
+			"url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FFatCross%2F0.0147_Sketch01Soft.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+			},
+		]
+	  },
+
+      "O 0147": {
+    "label": "O 0147",
+    "variants": [
+      {
+        "label": "O_0147_A_shaded_right.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_shaded_right.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_shaded_back.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_shaded_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_shaded_top.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_shaded_top.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_shaded_bottom.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_shaded_bottom.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_shaded_front.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_shaded_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_shaded_left.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_shaded_left.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_color_right.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_color_right.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_color_back.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_color_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_color_top.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_color_top.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_color_bottom.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_color_bottom.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_color_front.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_color_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_A_color_left.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FA%2FO_0147_A_color_left.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_shaded_right.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_shaded_right.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_shaded_back.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_shaded_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_shaded_top.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_shaded_top.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_shaded_bottom.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_shaded_bottom.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_shaded_front.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_shaded_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_shaded_left.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_shaded_left.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_color_right.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_color_right.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_color_back.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_color_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_color_top.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_color_top.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_color_bottom.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_color_bottom.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_color_front.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_color_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_B_color_left.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FB%2FO_0147_B_color_left.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_shaded_right.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_shaded_right.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_shaded_back.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_shaded_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_shaded_top.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_shaded_top.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_shaded_bottom.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_shaded_bottom.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_shaded_front.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_shaded_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_shaded_left.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_shaded_left.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_color_right.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_color_right.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_color_back.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_color_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_color_top.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_color_top.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_color_bottom.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_color_bottom.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_color_front.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_color_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_C_color_left.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FC%2FO_0147_C_color_left.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_shaded_right.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_shaded_right.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_shaded_back.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_shaded_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_shaded_top.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_shaded_top.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_shaded_bottom.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_shaded_bottom.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_shaded_front.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_shaded_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_shaded_left.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_shaded_left.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_color_right.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_color_right.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_color_back.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_color_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_color_top.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_color_top.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_color_bottom.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_color_bottom.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_color_front.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_color_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_D_color_left.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FD%2FO_0147_D_color_left.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_soft_right.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_soft_right.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_soft_back.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_soft_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_soft_top.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_soft_top.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_soft_bottom.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_soft_bottom.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_soft_front.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_soft_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_soft_left.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_soft_left.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_hard_right.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_hard_right.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_hard_back.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_hard_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_hard_top.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_hard_top.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_hard_bottom.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_hard_bottom.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_hard_front.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_hard_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      },
+      {
+        "label": "O_0147_sketch_hard_left.png",
+        "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FO_0147%2FSketch%2FO_0147_sketch_hard_left.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+      }
+    ]
+  },
+  "TCH92":{
+          "label":"TCH92",
+          "variants":[{
+            	"label": "TCH92_photo_front.png",
+              "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FTCH92%2FTCH92_photo_front.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+          },
+          {
+            	"label": "TCH92_photo_back.png",
+              "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FTCH92%2FTCH92_photo_back.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+          },
+          {
+            	"label": "TCH92_auto.png",
+              "url": "https://gitlab.rlp.net/api/v4/projects/28015/repository/files/renderings%2FTCH92%2FTCH92_auto.png/raw?ref=master&access_token=HpHNEWDQGcqbzyWsKTR4"
+          }
+          ]
+        }
+}
+
