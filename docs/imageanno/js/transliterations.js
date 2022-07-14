7e7cc4be883b10d5a2a58e22a889be462a305902
situx <timo.homburg@gmx.de>
2022-07-14 02:26:07 +0200

    Prepared image annos

---
+++ docs/imageanno/js/transliterations.js
@@ -0,0 +1,3 @@
+var transliterations={
+      "HS_1174": "@tablet\n@obverse \n1. 3(u) 3(disz) gurusz szu dim2-ma \n2. da-ak-ru-um e2-a ib2-dab5 \n3. ugula szesz-da-da \n4. 9(disz) gurusz szu dim2-ma \n@reverse \n1. ugula lu2-ga-a \n2. nu-banda3 ku3-sa6-ga \n3. e2-gal ensi2-ka \n4. sza3 tum-al!(TA) \n6. iti du6-ku3 u4 9(disz) "
+}
