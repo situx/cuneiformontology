6bc46951db73930a2f10272fdd639c505f7629be
Timo <situx@users.noreply.github.com>
2022-08-11 21:07:18 +0200

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -1056,7 +1056,7 @@ class OntDocGeneration:
                     "{{baseurl}}", prefixnamespace).replace(
                     "{{scriptfolderpath}}", outpath + corpusid + '_search.js').replace("{{indexpage}}","true")
             indexhtml+="<p>This page shows information about linked data resources in HTML. Choose the classtree navigation or search to browse the data</p>"
-            indexhtml+="<table class=\"description\" style =\"height: 100%; overflow: auto\" border=1 id=indextable><thead><tr><th>Class</th><th>Number of instances</th><th><th>Instance Example</th></tr></thead><tbody>"
+            indexhtml+="<table class=\"description\" style =\"height: 100%; overflow: auto\" border=1 id=indextable><thead><tr><th>Class</th><th>Number of instances</th><th>Instance Example</th></tr></thead><tbody>"
             for item in tree["core"]["data"]:
                 if (item["type"]=="geoclass" or item["type"]=="class" or item["type"]=="featurecollection" or item["type"]=="geocollection") and "instancecount" in item and item["instancecount"]>0:
                     indexhtml+="<tr><td><img src=\""+tree["types"][item["type"]]["icon"]+"\" height=\"25\" width=\"25\" alt=\""+item["type"]+"\"/><a href=\""+str(item["id"])+"\" target=\"_blank\">"+str(item["text"])+"</a></td>"
