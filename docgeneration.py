ac3a96a472d236c8159c6ad6599f59d7a4e9c612
Timo <situx@users.noreply.github.com>
2022-08-13 02:35:45 +0200

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -1103,7 +1103,7 @@ class OntDocGeneration:
             indexhtml+="<p>This page shows information about linked data resources in HTML. Choose the classtree navigation or search to browse the data</p>"
             indexhtml+="<table class=\"description\" style =\"height: 100%; overflow: auto\" border=1 id=indextable><thead><tr><th>Class</th><th>Number of instances</th><th>Instance Example</th></tr></thead><tbody>"
             for item in tree["core"]["data"]:
-                if (item["type"]=="geoclass" or item["type"]=="class" or item["type"]=="featurecollection" or item["type"]=="geocollection") and "instancecount" in item and item["instancecount"]>0:
+                if (nslink==prefixnamespace or nslink in item["id"]) and (item["type"]=="geoclass" or item["type"]=="class" or item["type"]=="featurecollection" or item["type"]=="geocollection") and "instancecount" in item and item["instancecount"]>0:
                     indexhtml+="<tr><td><img src=\""+tree["types"][item["type"]]["icon"]+"\" height=\"25\" width=\"25\" alt=\""+item["type"]+"\"/><a href=\""+str(item["id"])+"\" target=\"_blank\">"+str(item["text"])+"</a></td>"
                     indexhtml+="<td>"+str(item["instancecount"])+"</td>"
                     for item2 in tree["core"]["data"]:
