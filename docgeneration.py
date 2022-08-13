bff7f9975a628e86b334153e5d3a25513f8d0f6e
Timo <situx@users.noreply.github.com>
2022-08-13 03:04:29 +0200

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -1103,11 +1103,9 @@ class OntDocGeneration:
             indexhtml+="<p>This page shows information about linked data resources in HTML. Choose the classtree navigation or search to browse the data</p>"
             indexhtml+="<table class=\"description\" style =\"height: 100%; overflow: auto\" border=1 id=indextable><thead><tr><th>Class</th><th>Number of instances</th><th>Instance Example</th></tr></thead><tbody>"
             for item in tree["core"]["data"]:
-                if (nslink==prefixnamespace or nslink in item["id"]) and (item["type"]=="geoclass" or item["type"]=="class" or item["type"]=="featurecollection" or item["type"]=="geocollection") and "instancecount" in item and item["instancecount"]>0:
+                if (item["type"]=="geoclass" or item["type"]=="class" or item["type"]=="featurecollection" or item["type"]=="geocollection") and "instancecount" in item and item["instancecount"]>0:
                     exitem=None
                     for item2 in tree["core"]["data"]:
-                        print(item2["id"])
-                        print(nslink)
                         if item2["parent"]==item["id"] and nslink in item2["id"]:
                             checkdepth = self.checkDepthFromPath(path, prefixnamespace, item2["id"])-1
                             exitem="<td><img src=\""+tree["types"][item2["type"]]["icon"]+"\" height=\"25\" width=\"25\" alt=\""+item2["type"]+"\"/><a href=\""+self.generateRelativeLinkFromGivenDepth(prefixnamespace,checkdepth,str(item2["id"]),True)+"\">"+str(item2["text"])+"</a></td>"
