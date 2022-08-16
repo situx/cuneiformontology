d8f748c8cd45a18d1c40b666f80b7d008fbf5837
Timo <situx@users.noreply.github.com>
2022-08-16 18:22:35 +0200

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -941,18 +941,18 @@ htmltemplate = """<html about=\"{{subject}}\"><head><title>{{toptitle}}</title>
 
 imagestemplate="""
 <div class="image">
-<img src="{{image}}" style="max-width:500px;max-height:500px" alt="{{image}}" title="{{imagetitle}}" />
+<img src="{{image}}" style="max-width:33.33%;max-height:500px" alt="{{image}}" title="{{imagetitle}}" />
 </div>
 """
 
 imageswithannotemplate="""<div class="image">
-<img src="{{image}}" style="max-width:500px;max-height:500px" alt="{{image}}" title="{{imagetitle}}" />
+<img src="{{image}}" style="max-width:33.33%;max-height:500px" alt="{{image}}" title="{{imagetitle}}" />
 {{svganno}}
 </div>
 """
 
 imagestemplatesvg="""
-<div class="image" style="max-width:500px;max-height:500px">
+<div class="image" style="max-width:33.33%;max-height:500px">
 {{image}}
 </div>
 """
@@ -1764,7 +1764,6 @@ class OntDocGeneration:
                     "{{scriptfolderpath}}", rellink).replace("{{classtreefolderpath}}", rellink2).replace("{{exports}}",myexports).replace("{{subject}}",str(subject)))
             if comment!=None:
                 f.write(htmlcommenttemplate.replace("{{comment}}",comment))
-            print(foundmedia)
             if len(foundmedia["mesh"])>0:
                 print("Found 3D Model: "+str(foundmedia["mesh"]))
                 for curitem in foundmedia["mesh"]:
