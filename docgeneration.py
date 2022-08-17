0ea1b5f1943111ffa30e8cc46bfc692f847f2953
Timo <situx@users.noreply.github.com>
2022-08-17 21:44:25 +0200

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -1324,21 +1324,21 @@ class OntDocGeneration:
         subtorencounter = 0
         for subj in subjectstorender:
             path = subj.replace(prefixnamespace, "")
-            try:
-                paths=self.processSubjectPath(outpath,paths,path)
-                if os.path.exists(outpath + path+"/index.ttl"):
-                    try:
-                        self.graph.parse(outpath + path+"/index.ttl")
-                    except Exception as e:
-                        print(e)
-                self.createHTML(outpath + path, self.graph.predicate_objects(subj), subj, prefixnamespace, self.graph.subject_predicates(subj),
-                           self.graph,str(corpusid) + "_search.js", str(corpusid) + "_classtree.js",uritotreeitem,curlicense,subjectstorender,postprocessing)
-                subtorencounter += 1
-                if subtorencounter%500==0:
-                    subtorenderlen=len(subjectstorender)+len(postprocessing)
-                print(str(subtorencounter) + "/" + str(subtorenderlen) + " " + str(outpath + path))
-            except Exception as e:
-                print(e)
+            #try:
+            paths=self.processSubjectPath(outpath,paths,path)
+            if os.path.exists(outpath + path+"/index.ttl"):
+                try:
+                    self.graph.parse(outpath + path+"/index.ttl")
+                except Exception as e:
+                    print(e)
+            self.createHTML(outpath + path, self.graph.predicate_objects(subj), subj, prefixnamespace, self.graph.subject_predicates(subj),
+                       self.graph,str(corpusid) + "_search.js", str(corpusid) + "_classtree.js",uritotreeitem,curlicense,subjectstorender,postprocessing)
+            subtorencounter += 1
+            if subtorencounter%500==0:
+                subtorenderlen=len(subjectstorender)+len(postprocessing)
+            print(str(subtorencounter) + "/" + str(subtorenderlen) + " " + str(outpath + path))
+            #except Exception as e:
+            #    print(e)
             #    #QgsMessageLog.logMessage("Exception occured " + str(e), "OntdocGeneration", Qgis.Info)
         print("Postprocessing " + str(len(postprocessing)))
         for subj in postprocessing.subjects():
