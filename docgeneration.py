ae22b9334803ae333b1aed30445d6fc89acf1472
Timo <situx@users.noreply.github.com>
2022-08-18 17:24:08 +0200

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -596,7 +596,7 @@ function initThreeJS(domelement,verts) {
     //renderer.setSize( window.innerWidth, window.innerHeight );
     document.getElementById(domelement).appendChild( renderer.domElement );
 	controls = new THREE.TrackballControls( camera, renderer.domElement );
-    controls.target.set(0, 0, 0);
+    controls.target.set(((maxx+minx)/2), ((maxy+miny)/2), ((maxz+minz)/2));
     //controls.maxDistance= Math.max(maxx, maxy, maxz)*4
     animate()
 }
