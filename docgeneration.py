9a7319b5ba5098855ccc77e079af253af526b2e9
Timo <situx@users.noreply.github.com>
2022-08-18 15:11:11 +0200

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -1054,9 +1054,7 @@ Your browser does not support the audio element.
 threejstemplate="""
 <div id="threejs" class="threejscontainer" style="max-width:485px;max-height:500px">
 </div>
-<script>
-initThreeJS('threejs',parseWKTStringToJSON("{{wktstring}}"))
-</script>
+<script>$(document).ready(function(){initThreeJS('threejs',parseWKTStringToJSON("{{wktstring}}"))})</script>
 """
 
 image3dtemplate="""<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/cnr-isti-vclab/3DHOP@4.3/minimal/stylesheet/3dhop.css"/>
@@ -1194,7 +1192,7 @@ htmltabletemplate="""
 htmlfooter="""<div id="footer"><div class="container-fluid"><b>Download Options:</b>&nbsp;Format:<select id="format" onchange="changeDefLink()">	
 {{exports}}
 </select><a id="formatlink2" href="#" target="_blank"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-info-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/></svg></a>&nbsp;
-<button id="downloadButton" onclick="download()">Download</button>{{license}}</div></div></body><script>setSVGDimensions()</script></html>"""
+<button id="downloadButton" onclick="download()">Download</button>{{license}}</div></div></body><script>$(document).ready(function(){setSVGDimensions()})</script></html>"""
 
 licensetemplate=""""""
 
