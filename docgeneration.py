cc5a22fd94d6704117195a56bfd3f88261f91742
Timo <situx@users.noreply.github.com>
2022-08-20 01:24:31 +0200

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -580,7 +580,6 @@ function initThreeJS(domelement,verts) {
         }
     }
     camera = new THREE.PerspectiveCamera(90,window.innerWidth / window.innerHeight, 0.01, 10 );
-    camera.position.z = maxz;
     var axesHelper = new THREE.AxesHelper( Math.max(maxx, maxy, maxz)*4 );
     scene.add( axesHelper );
     console.log("Depth: "+(maxz-minz))
@@ -600,6 +599,7 @@ function initThreeJS(domelement,verts) {
     controls.target.copy( mesh.position );
     controls.update();
     controls.target.set(centervec.x, centervec.y, centervec.z);
+    camera.position.z = centervec.z+maxz;
     controls.maxDistance= Math.max(maxx, maxy, maxz)*2
     animate()
 }
