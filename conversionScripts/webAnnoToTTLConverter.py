69203c7dba8868b0628236ce5f23937909b7336f
Timo <situx@users.noreply.github.com>
2022-09-05 18:55:33 +0200

    reference updates

--- conversionScripts/webAnnoToTTLConverter.py
+++ conversionScripts/webAnnoToTTLConverter.py
@@ -18,6 +18,7 @@ filename="../examples/"+str(tabletname)+"/ttl/"+str(tabletname)+"_"+str(origtabl
 withlines=False
 withcharoccs=False
 withglyphs=False
+compreftypes={"PCA":"http://purl.org/meshsparql#PCAReference"}
 
 def coptoRDF(fw,crsnamespace,indid,opaswkt,induid):
     induuid=induid.replace("#","")
@@ -226,11 +227,18 @@ for tabname in tabletnames:
                         comprefid=str(indid)+"_target3d_selector_compref"+str(i)
                         print(compref)
                         res.write("<"+str(indid)+"_target3d_selector> <http://purl.org/meshsparql#computingReference> <"+str(comprefid)+"> . \n")
-                        res.write("<"+str(comprefid)+"> rdf:type <http://purl.org/meshsparql#ComputingReference> .\n")
+                        if "type" in compref:
+                            if compref["type"] in compreftypes:
+                                res.write("<"+str(compreftypes[compref["type"]])+"> rdfs:subClassOf <http://purl.org/meshsparql#ComputingReference> .\n")
+                                res.write("<"+str(comprefid)+"> rdf:type \""+str(compreftypes[compref["type"]])+"\"^^xsd:string .\n")
+                            else:
+                                res.write("<"+str(comprefid)+"> rdf:type <http://purl.org/meshsparql#ComputingReference> .\n")
+                                res.write("<"+str(comprefid)+"> <http://purl.org/meshsparql#comprefType> \""+str(compref["type"])+"\"^^xsd:string .\n")   
+                        else:
+                            res.write("<"+str(comprefid)+"> rdf:type <http://purl.org/meshsparql#ComputingReference> .\n")
                         res.write("<"+str(comprefid)+"> rdfs:label \"Computing Reference of 3D Annotation target selector of Annotation of Glyph at "+str(tabname)+" "+str(tabletside)+" line "+str(lineindex)+" char "+str(charindex)+" on a 3D Mesh\" .\n")
                         res.write("<"+str(comprefid)+"> <http://purl.org/meshsparql#stable> \""+str(compref["stable"])+"\"^^xsd:boolean .\n")
-                        res.write("<"+str(comprefid)+"> <http://purl.org/meshsparql#transformationMatrix> \""+str(compref["transformationmatrix"]).replace("\n","").replace("\\n","")+"\"^^xsd:string .\n")
-                        res.write("<"+str(comprefid)+"> <http://purl.org/meshsparql#comprefType> \""+str(compref["type"])+"\"^^xsd:string .\n")
+                        res.write("<"+str(comprefid)+"> <http://purl.org/meshsparql#transformationMatrix> \""+str(compref["transformationmatrix"]).replace("\n","").replace("\\n","")+"\"^^msp:matrixLiteral .\n")
                         res.write("<"+str(comprefid)+"> <http://purl.org/meshsparql#referenceVector> \"\"\""+str(compref["value"]).replace("\r","").replace("\n","").replace("\\n","")+"\"\"\"^^xsd:string .\n")
                         res.write("<"+str(comprefid)+"> <http://purl.org/meshsparql#wktTransformation> \"\"\""+str(compref["wktTransformation"]).replace("\n","").replace("\"","\\\"")+"\"\"\"^^geocrs:wktLiteral .\n")
                         coptoRDF(res,crsnamespace,str(comprefid),"\"\"\""+str(compref["wktTransformation"]).replace("\n","").replace("\"","\\\"")+"\"\"\"",key)
