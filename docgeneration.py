6f927e8dbab48859cda8ad8baeb801e8d9906d1a
Timo <situx@users.noreply.github.com>
2022-09-08 20:29:07 +0200

    Update docgeneration.py

--- docgeneration.py
+++ docgeneration.py
@@ -1690,11 +1690,11 @@ class OntDocGeneration:
                 elif str(tup[0])!="http://www.w3.org/ns/oa#hasTarget":
                     for inttup in graph.predicate_objects(tup[1]):
                         if str(inttup[0])=="http://www.w3.org/ns/oa#hasSelector":
-                        for valtup in graph.predicate_objects(inttup[1]):
-                            if str(valtup[0]) in unitproperties:
-                                foundunit=str(valtup[1])
-                            if str(valtup[0]) in valueproperties and (isinstance(valtup[1],Literal) or isinstance(valtup[1],URIRef)):
-                                foundval=str(valtup[1])  
+                            for valtup in graph.predicate_objects(inttup[1]):
+                                if str(valtup[0]) in unitproperties:
+                                    foundunit=str(valtup[1])
+                                if str(valtup[0]) in valueproperties and (isinstance(valtup[1],Literal) or isinstance(valtup[1],URIRef)):
+                                    foundval=str(valtup[1])  
                 else:
                     for valtup in graph.predicate_objects(tup[1]):
                         if str(valtup[0]) in unitproperties:
